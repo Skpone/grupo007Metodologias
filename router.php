7c22b5d04171c1a5e49ab7326119e0fe0d1e320c
Romeo Larrondo <larrondoromeo@gmail.com>
2022-11-07 21:08:42 -0300

    optimizacion codigo

--- router.php
+++ router.php
@@ -16,38 +16,34 @@ if (!empty($_GET['action'])) {
 
 $params = explode('/', $action);
 
+$medicController = new MedicController();
+$secretaryController = new SecretaryController();
+
+
 switch ($params[0]) {
     case 'admin':
-        $medicController = new MedicController();
         $medicController->showFirstPage();
 
         break;
     case 'nuevoMedico':
-        $medicController = new MedicController();
         $medicController->showNewMedicForm();
         break;
     case 'nuevaSecretaria':
-        $secretaryController = new SecretaryController();
         $secretaryController->showNewSecretaryForm();
         break;
     case 'agregarMedico':
-        $medicController = new MedicController();
         $medicController->addMedic();
         break;
     case 'eliminarSecretaria':
-        $secretaryController = new SecretaryController();
         $secretaryController->eraseSecretary($params[1]);
         break;
     case 'eliminarMedico':
-        $secretaryController = new MedicController();
-        $secretaryController->eraseMedic($params[1]);
+        $medicController->eraseMedic($params[1]);
         break;
     case 'secretarias':
-        $secretaryController = new SecretaryController();
         $secretaryController->displaySecretariesList();
         break;
     case 'medicos':
-        $medicController = new MedicController();
         $medicController->displayMedicsList();
         break;
     default:
